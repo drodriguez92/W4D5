317522af0f34d0ffe1221607251467871fb0e7cf
That Guy <abc@gmail.com>
2018-10-19 13:58:11 -0400

    Create User Table

---
+++ social_app/spec/models/user_spec.rb
@@ -0,0 +1,21 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint(8)        not null, primary key
+#  username        :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
+require 'rails_helper'
+
+describe User do
+  it { should validate_presence_of(:username) }
+  it { should validate_presence_of(:password_digest) }
+  it { should validate_presence_of(:session_token) }
+  it { should validate_uniqueness_of(:username) }
+
+end
